94c3e9f38b900760e4e615c1d982d533fce436b6
juhyeon <juhyeon1114@gmail.com>
2023-02-02 16:59:08 +0900

    update : sh 작성

---
+++ scripts/start.sh
@@ -0,0 +1,17 @@
+ROOT_PATH="/home/ubuntu/cicdtest"
+JAR="$ROOT_PATH/application.jar"
+
+APP_LOG="$ROOT_PATH/log/application.log"
+ERROR_LOG="$ROOT_PATH/log/error.log"
+LOG_FILE="$ROOT_PATH/log/start.log"
+
+TIME_NOW=$(date +%c)
+
+echo "$TIME_NOW > $JAR 복사" >> $LOG_FILE
+cp $ROOT_PATH/build/libs/*.jar $JAR
+
+echo "$TIME_NOW > $JAR 실행" >> $LOG_FILE
+nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+
+SERVICE_PID=$(pgrep -f $JAR)
+echo "$TIME_NOW > 서비스 PID: $SERVICE_PID" >> $LOG_FILE
---
+++ scripts/stop.sh
@@ -0,0 +1,12 @@
+ROOT_PATH="/home/ubuntu/cicdtest"
+JAR="$ROOT_PATH/application.jar"
+LOG_FILE="$ROOT_PATH/log/stop.log"
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+
+if [ -z "$SERVICE_PID" ]; then
+  echo "서비스 NouFound" >> LOG_FILE
+else
+  echo "서비스 종료 " >> LOG_FILE
+  kill "$SERVICE_PID"
+  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
+fi
