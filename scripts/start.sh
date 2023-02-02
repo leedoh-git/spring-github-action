b8d65413eef1390251969b75a0f364ef191a3a70
juhyeon <juhyeon1114@gmail.com>
2023-02-02 19:39:08 +0900

    update : Scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,17 +3,17 @@
 ROOT_PATH="/home/ubuntu/spring-github-action"
 JAR="$ROOT_PATH/application.jar"
 
-APP_LOG="$ROOT_PATH/log/application.log"
-ERROR_LOG="$ROOT_PATH/log/error.log"
-LOG_FILE="$ROOT_PATH/log/start.log"
+APP_LOG="$ROOT_PATH/application.log"
+ERROR_LOG="$ROOT_PATH/error.log"
+START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $LOG_FILE
+echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/spring-github-action-1.0.0.jar $JAR
 
-echo "[$NOW] > $JAR 실행" >> $LOG_FILE
+echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $LOG_FILE
+echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
